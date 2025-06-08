b6bde6db8c4ca6b3466a704b9db127e2649521c1
Oleg Sopyak <49557238+vegetab1e@users.noreply.github.com>
2025-06-08 14:18:50 +0300

    Initial commit

---
+++ io.h
@@ -0,0 +1,125 @@
+﻿#pragma once
+
+#include <array>
+#include <vector>
+#include <string>
+
+#include <fstream>
+
+#include <stdexcept>
+
+#include <nlohmann/json.hpp>
+
+#include "point.h"
+
+template<class C, class V, std::size_t N>
+std::vector<Point<C, V, N>> readPoints(const std::string& filename,
+                                       const std::array<const char*, N>& axis_names,
+                                       const char* value_name)
+{
+    using json = nlohmann::json;
+
+    std::ifstream file{filename};
+    if (!file.is_open())
+        return {};
+
+    std::vector<Point<C, V, N>> points;
+
+    try
+    {
+        const json data = json::parse(file);
+        if (!data.is_array() || data.empty())
+            throw std::logic_error("The file is ill-formed!");
+
+        points.reserve(data.size());
+
+        C coords[N]{};
+        for (const json& object : data)
+        {
+            if (!object.is_object() || object.size() < N)
+                throw std::logic_error("The array is invalid!");
+
+            json::const_iterator iterator;
+            for (std::size_t i = 0; i < N; ++i)
+            {
+                iterator = object.find(axis_names[i]);
+                if (iterator == object.cend() || !iterator->is_number())
+                    throw std::logic_error("The coordinate is missing!");
+
+                coords[i] = iterator.value().template get<C>();
+            }
+
+            V value{};
+            iterator = object.find(value_name);
+            if (iterator != object.cend() && iterator->is_number())
+                value = iterator.value().template get<V>();
+
+            points.emplace_back(coords, value);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+
+        points.clear();
+    }
+
+    file.close();
+
+    return points;
+}
+
+// Вызов данной функции выполняется так:
+// readPoints<decltype(points[0])>(...);
+// decltype(points[0]) — это тип объекта, например, Point<int, double, 2>,
+// а не сам шаблон Point, поэтому невозможно принять его подобным образом:
+// template<template<class, class, std::size_t> class Point, class C, class V, std::size_t N>
+// Есть два способа решения проблемы: разбирать тип объекта с помощью вспомогательных функций
+// или структур с соответствующими параметрами шаблонов, как здесь, или же, что будет гораздо
+// проще, добавить соответствующий аргумент со значением по умолчанию в функцию readPoints().
+// Варианта с передачей вектора для заполнения внутрь функции readPoints() не рассматривался,
+// потому что тогда волшебство исчезнет и карета превратится в тыкву=(
+template<class PointType, class... Types>
+auto readPoints(Types&&... arguments)
+{
+    return readPoints<GetParamAt<0, PointType>,
+                      GetParamAt<1, PointType>,
+                      GetParamAt<2, PointType>::value>(
+                          std::forward<Types>(arguments)...);
+}
+
+template<class C, class V, std::size_t N>
+void writePoints(const std::vector<Point<C, V, N>>& points,
+                 const std::string& filename,
+                 int json_indent,
+                 const std::array<const char*, N>& axis_names,
+                 const char* value_name)
+{
+    using json = nlohmann::json;
+
+    std::ofstream file{filename};
+    if (!file.is_open())
+        return;
+
+    try
+    {
+        json array = json::array();
+        for (auto& point : points)
+        {
+            json object = json::object();
+            for (std::size_t i = 0; i < N; ++i)
+                object[axis_names[i]] = point.getCoord(i);
+            object[value_name] = point.getValue();
+
+            array.emplace_back(std::move(object));
+        }
+
+        file << array.dump(json_indent);
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    file.close();
+}
---
+++ utils.h
@@ -0,0 +1,144 @@
+﻿#pragma once
+
+#include <type_traits>
+
+#include "type_cast.h"
+
+template<class T>
+inline constexpr T EPSILON = T(1.0E-8L);
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+// Этот макрос нужен, чтобы избежать ошибки C4146:
+// "unary minus operator applied to unsigned type,
+// result still unsigned", когда других вариантов,
+#define ABS_EX(x) ((x) < 0 ? negateValue(x) : (x))
+// например, constexpr if statement, просто нет.
+
+#if __cplusplus < 201703L
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if<std::is_signed<Type>::value, Type>::type
+negateValue(Type value) noexcept
+{
+    return -value;
+}
+
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if<std::is_unsigned<Type>::value, Type>::type
+negateValue(Type value) noexcept
+{
+    return value;
+}
+#else
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if_t<std::is_arithmetic_v<Type>, Type>
+negateValue(Type value) noexcept
+{
+    if constexpr (std::is_signed_v<Type>)
+        return -value;
+    else
+        return value;
+}
+#endif
+
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if_t<std::is_integral_v<Type>, bool>
+isZero(Type x) noexcept
+{
+    return x == Type(0);
+}
+
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if_t<std::is_floating_point_v<Type>, bool>
+isZero(Type x) noexcept
+{
+    return ABS(x) < EPSILON<Type>;
+}
+
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if_t<std::is_integral_v<Type>, bool>
+isEqual(Type x, Type y) noexcept
+{
+    return x == y;
+}
+
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if_t<std::is_floating_point_v<Type>, bool>
+isEqual(Type x, Type y) noexcept
+{
+    // Сравнивать переменные между собой и с нулём напрямую можно, если их
+    // значения получены прямым присвоением одинаковых литералов, без иных
+    // манипуляций, таких как математические операции или привидение типов
+    // (иногда приведение допустимо, а в случае с нулём допустимо всегда).
+    const auto diff = static_cast<TwiceBiggerType<Type>>(x) - y;
+    return ABS(diff) < EPSILON<decltype(diff)>;
+}
+
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if_t<std::is_arithmetic_v<Type>,
+SignedType<TwiceBiggerType<Type>>>
+toSignedTwiceBiggerArithmeticType(Type x) noexcept
+{
+    return static_cast<SignedType<TwiceBiggerType<Type>>>(static_cast<TwiceBiggerType<Type>>(x));
+}
+
+template<class Type>
+inline
+#ifdef __GNUC__
+__attribute__((always_inline))
+#else
+__forceinline
+#endif
+std::enable_if_t<std::is_arithmetic_v<Type>,
+UnsignedType<BiggestType<Type>>>
+toUnsignedBiggestArithmeticType(Type x) noexcept
+{
+    return static_cast<UnsignedType<BiggestType<Type>>>(static_cast<UnsignedType<Type>>(ABS_EX(x)));
+}
