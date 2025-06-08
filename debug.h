172b4e2aef6ac72f1caed0ac5b837618f97fec4a
Oleg Sopyak <49557238+vegetab1e@users.noreply.github.com>
2025-06-08 14:18:50 +0300

    Initial commit

---
+++ debug.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <iostream>
+
+inline void printDebugInfo(const char* func_sig,
+                           const char* filename,
+                           int line_no,
+                           ...)
+{
+    std::cout << "\x1b[1mFUNC: \x1b[0m" << func_sig << '\n'
+              << "\x1b[1mFILE: \x1b[0m" << filename << '\n'
+              << "\x1b[1mLINE: \x1b[0m" << line_no
+              << std::endl;
+}
+
+#if defined(_MSC_VER)
+#define DEBUG_INFO(...) printDebugInfo(__FUNCSIG__, __FILE__, __LINE__, __VA_ARGS__)
+#elif defined(__GNUC__)
+#define DEBUG_INFO(...) printDebugInfo(__PRETTY_FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#error Failing compilation
+#endif
---
+++ tools.h
@@ -0,0 +1,103 @@
+﻿#pragma once
+
+#include <cmath>
+
+#include <array>
+#include <vector>
+#include <string>
+
+#include <exception>
+
+#include <nlohmann/json.hpp>
+
+#ifndef NDEBUG
+#include <filesystem>
+#include "io.h"
+#endif
+
+#include "kdtree.h"
+#include "point.h"
+#include "utils.h"
+
+template<class C, class V, std::size_t N>
+V shepardInterpolation(const Point<C, V, N>& point,
+                       const std::vector<Point<C, V, N>>& neighbors,
+                       double idw_power = 2.0) noexcept
+{
+    long double num = 0.0L, den = 0.0L;
+    for (const auto& neighbor : neighbors)
+    {
+        const auto distance = neighbor.getDistance(point);
+#ifdef ZERO_DISTANCE_HANDLING
+        if (isZero(distance)) [[unlikely]]
+            return neighbor.getValue();
+
+        const auto weight = 1.0 / std::pow(distance, idw_power);
+#else
+        const auto weight = 1.0 / std::pow(isZero(distance) ? EPSILON<decltype(distance)>
+                                                            : distance,
+                                           idw_power);
+#endif
+        num += weight * neighbor.getValue();
+        den += weight;
+    }
+
+    if constexpr (!std::is_same_v<decltype(num), V>)
+        return static_cast<V>(num / den);
+    else
+        return num / den;
+}
+
+template<class C, class V, std::size_t N>
+std::string shepardInterpolation(const KdTree<Point<C, V, N>>& tree,
+                                 std::vector<Point<C, V, N>>& points,
+                                 std::size_t num_neighbors,
+                                 bool reverse_search,
+                                 double idw_power,
+                                 int json_indent,
+                                 const std::array<const char*, N>& axis_names,
+                                 const char* value_name) noexcept
+try
+{
+    using json = nlohmann::json;
+
+#ifndef NDEBUG
+    std::string path{"out/"};
+    path += reverse_search ? "rnns/" : "nns/";
+    std::filesystem::create_directories(path);
+#endif
+
+    json array = json::array();
+    for (auto& point : points)
+    {
+#ifndef NDEBUG
+        auto neighbors = 
+#endif
+        tree.shepardInterpolation(point,
+                                  num_neighbors,
+                                  reverse_search,
+                                  idw_power);
+#ifndef NDEBUG
+        writePoints(path + point.toString() + ".json",
+                    neighbors,
+                    json_indent,
+                    axis_names,
+                    value_name);
+#endif
+
+        json object = json::object();
+        for (std::size_t i = 0; i < N; ++i)
+            object[axis_names[i]] = point.getCoord(i);
+        object[value_name] = point.getValue();
+
+        array.emplace_back(std::move(object));
+    }
+
+    return array.dump(json_indent);
+}
+catch (const std::exception& e)
+{
+    std::cout << e.what() << std::endl;
+
+    return {};
+}
